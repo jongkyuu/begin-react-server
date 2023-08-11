3d1a32fc5f9e4aff610ce8fbe7f11b399fe15888
jongkyuu <minerva421.ai@gmail.com>
2023-08-11 23:55:21 +0900

    app.js 수정

--- app.js
+++ app.js
@@ -9,6 +9,37 @@ app.get("/api/hello", (req, res) => {
     res.send({ message: "Hello World!" });
 });
 
+app.get("/api/customers", (req, res) => {
+    res.send({
+        customers: [
+            {
+                id: 1,
+                image: "https://picsum.photos/id/1/64/64",
+                name: "John Doe",
+                birthday: "1985-07-15",
+                gender: "Male",
+                job: "Software Engineer",
+            },
+            {
+                id: 2,
+                image: "https://picsum.photos/id/2/64/64",
+                name: "Jane Smith",
+                birthday: "1990-03-22",
+                gender: "Female",
+                job: "Marketing Manager",
+            },
+            {
+                id: 3,
+                image: "https://picsum.photos/id/3/64/64",
+                name: "Alex Johnson",
+                birthday: "1978-11-05",
+                gender: "Male",
+                job: "Teacher",
+            },
+        ],
+    });
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
